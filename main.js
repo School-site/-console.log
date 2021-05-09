a458c28b9dfb259bb234bb2d50c67a4d6e9d80a5
School-site <76882788+School-site@users.noreply.github.com>
2021-05-09 12:47:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var Firstnamee= "HemantKumar";
+var lastnamee= "Thareja";
+var Fullnamee= Firstnamee.concat(lastnamee);
+console.log(Fullnamee);
+
+
+var a=100;
+var b=1000;
+var add=a+b;
+console.log(add);
+
+
+var c=900;
+var d=9000;
+var sub=d-c;
+console.log(sub);
+
+
+var e=8000;
+var f=80000;
+var div=f/e;
+console.log(div);
+
+
+var g=70000;
+var h=700000;
+var mult=h*g;
+console.log(mult);
